f0629e23160026ffac182de3a0220b3d98f73ce3
camkirk <camden.kirkland@gmail.com>
2018-10-08 00:13:09 -0500

    message-boundary

---
+++ networking/netWatcherJsonClient.js
@@ -0,0 +1,19 @@
+'use strict';
+const net = require('net');
+const client = net.connect({port:8000});
+
+client.on('data', (data)=>{
+    const message = JSON.parse(data);
+    switch (message.type){
+        case 'watching':
+            console.log(`now watching ${message.file}`);
+            break;
+        case 'changed':
+            const date = new Date(message.timestamp);            console.log(`file changed at ${date}`);
+            break;
+        default:
+            console.log("Unrecognized message type: ${message.type}");
+    }
+
+            
+}).on('error',(err)=>console.log(err));
---
+++ networking/testJsonService.js
@@ -0,0 +1,24 @@
+'use strict';
+const server = require('net').createServer((connection)=>{
+    console.log('sub connected');
+    
+    const chunkOne = '{
+        "type":"changed",
+        "timesta';
+    
+    const chunkTwo = 'mp":1538975350426}';
+
+    connection.write(chunkOne);
+
+    const timer = setTimeout(()=>{
+        connection.write(chunkTwo);
+        connection.end();
+    }, 100);
+
+    connection.on('end',()=>{
+        clearTimeout(timer);
+        console.log('sub disconnect');
+        
+    });
+}).listen(8000,()=>console.log('port listening for subs'));
+
