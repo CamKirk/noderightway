137456e9a66d325265e47810d8be49c70a0d965d
camkirk <camden.kirkland@gmail.com>
2018-11-02 20:04:52 -0500

    finishing up networking

--- networking/lib/ldjClient.js
+++ networking/lib/ldjClient.js
@@ -6,15 +6,13 @@ class LDJClient extends EventEmitter{ //LDJ = line-delimited JSON
         let buffer = '';
         stream.on('data', (data)=> {
             buffer += data;
-            console.log(buffer);
             
             let boundary = buffer.indexOf('\n');
             while (boundary !== -1){
                 const input = buffer.substring(0, boundary);
                 buffer = buffer.substring(boundary+1);
-                console.log(buffer);
-                
-                this.emit('message', JSON.parse(input)); //returning an unexpected end of input? Looks like chunkOne is writing but chunkTwo is not.
+                // console.log(buffer);
+                this.emit('message', JSON.parse(input)); //returning an unexpected end of input? Looks like chunkOne is writing but chunkTwo is not. <-- Need to go back to testJSONService.js and add the \n back in as per instructions.
                 boundary = buffer.indexOf('\n');
                 
             }
