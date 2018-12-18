8cac7f22d8f59449cd00c84024f08c884800b84e
camkirk <camden.kirkland@gmail.com>
2018-12-18 12:24:22 -0600

    esclu first cli functions

---
+++ esclu/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const fs = require('fs');
+const request = require('request');
+const program = require('commander');
+const pkg = require('./package.json');
+
+program
+    .version(pkg.version)
+    .description(pkg.description)
+    .usage('[options] <command> [...]')
+    .option('-o, --host <hostname>', 'hostname [localhost]','localhost')
+    .option('-p, --port <number>', 'port number [9200]', '9200')
+    .option('-j, --json', 'format output as JSON')
+    .option('-i, --index  <name>', 'which index to use')
+    .option('-t, --type <type>', 'default type for bulk operations');
+
+const fullURL  = (path = '') => {
+    let url = `http://${program.host}:${program.port}/`
+    if(program.index){
+        url += program.index + '/';
+
+        if (program.type){
+            url+= program.type + '/';
+
+        }
+    }
+    return url + path.replace(/^|\*/,'');
+}
+
+program
+    .command('url [path]')
+    .description('generate the URL for the options and path (default is /)')
+    .action((path = '/') => console.log(fullURL(path)));
+    
+program
+    .command('get [path]')
+    .description('perform an HTTP GET request for path (defualt is /)')
+    .action((path = '/')=>{
+
+        const options = {
+            url: fullURL(path),
+            json: program.json
+        };
+
+        request(options, (err, res, body) => {
+            if (program.json){
+                console.log(JSON.stringify(err||body));
+                
+            }else{
+                if(err) throw err;
+                console.log(body);
+                
+            }
+            
+        })
+    })
+
+
+program.parse(process.argv);
+
+
+
+
+
+
+
+
+if(!program.args.filter((arg)=> typeof arg === 'object').length){
+    program.help();
+}
