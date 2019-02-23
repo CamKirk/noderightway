0830a59642b460987f82dfa5800efd6d0f85cd67
camkirk <camden.kirkland@gmail.com>
2019-02-23 02:58:51 -0600

    suggest route

--- web-services/b4/lib/search.js
+++ web-services/b4/lib/search.js
@@ -39,4 +39,39 @@ module.exports = (app, es) => {
                 .json(esResBody.hits.hits.map(({_source})=> _source))
         })
     })
+
+    app.get('/api/suggest/:field/:query', (req, res)=>{
+
+        const esReqBody = {
+            size: 0,
+            suggest:{
+                suggestions:{
+                    text: req.params.query,
+                    term: {
+                        field: req.params.field,
+                        suggest_mode: 'always'
+                    }
+                }
+            }
+        }
+
+        const options = {url, json:true, body:esReqBody}
+
+        const promise = new Promise((resolve, reject)=>{
+            request.get(options, (err, esRes, esResBody)=>{
+                if(err){
+                    return reject({error:err})
+                }
+                if(esRes.statusCode !== 200){
+                    return reject({error: esResBody})
+                }
+                resolve(esResBody);
+            })
+        })
+
+        promise
+            .then((esResBody)=> res.status(200).json(esResBody.suggest.suggestions))
+            .catch(({error})=> res.status(error.status || 502).json(error))
+
+    })
 }
